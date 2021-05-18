134a2865219dc4062aaab9a6d8dcad4c33dd8b27
Steve Galbraith <Steve.Galbraith@resmed.com>
2021-05-18 16:46:50 -0600

    Initial Commit

---
+++ ArchitectureSample/ArchitectureSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchitectureSampleApp.swift
+//  ArchitectureSample
+//
+//  Created by Steve Galbraith on 5/18/21.
+//
+
+import SwiftUI
+
+@main
+struct ArchitectureSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
